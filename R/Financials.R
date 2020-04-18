dbf62a79c094a7af89245dc8bb305447252257b6
Per Nyfelt <perNyfelt@users.noreply.github.com>
2020-04-18 10:08:39 +0200

    make param order consistent, add docs.

--- R/Financials.R
+++ R/Financials.R
@@ -1,30 +1,40 @@
 
 # Equivalent to Excel/Calc's PMT(interest_rate, number_payments, PV, FV, Type)
 # function, which calculates the payments for a loan or the future value of an investment
-# @param r    - periodic interest rate represented as a decimal.
+# @param interestRate    - periodic interest rate represented as a decimal.
 # @param nper - number of total payments / periods.
 # @param pv   - present value -- borrowed or invested principal.
-# @param fv   - future value of loan or annuity.
-# @param type - when payment is made: beginning of period is 1; end, 0.
-# @return <code>double</code> representing periodic payment amount.
-pmt <- function(r, nper, pv, fv = 0, type = 0) {
-  -r * (pv * ((1 + r)^ nper) + fv) / ((1 + r * type) * (((1 + r)^ nper) - 1))
+# @param fv   - future value of loan or annuity, default to 0 (which is what you want for loans)
+# @param type - when payment is made: beginning of period is 1; end is 0. Default is 0
+# @return double representing the periodic payment amount.
+pmt <- function(interestRate, nper, pv, fv = 0, type = 0) {
+  -interestRate * (pv * ((1 + interestRate)^ nper) + fv) / ((1 + interestRate * type) * (((1 + interestRate)^ nper) - 1))
 }
 
-
-monthlyAnnuityAmount <- function(loanAmount, interestRate, tenureMonths, amortizationFreemonths) {
+# the monthyl annuity amount i.e. the amortization and interest amount each payment period (month)
+# @param loanAmount the total loan amount including capitalized fees (e.g. startup fee)
+# @param interestRate the annual nominal interest
+# @param tenureMonths the tenure of the loan in number of months
+# @param amortizationFreeMonths the number of initial amortization free months, default to 0
+# @param type - when payment is made: beginning of period is 1; end is 0. Default is 0
+monthlyAnnuityAmount <- function(loanAmount, interestRate, tenureMonths, amortizationFreemonths = 0, type = 0) {
   montlyInterest <- interestRate / 12
   totalNumberOfPaymentPeriods <- tenureMonths - amortizationFreemonths
-  pmt(montlyInterest, totalNumberOfPaymentPeriods, loanAmount * -1)
+  pmt(montlyInterest, totalNumberOfPaymentPeriods, loanAmount * -1, type)
 }
 
 # return a vector of cachFlow entries for each period
-cashFlow <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoiceFee) {
-  interestCostAmFreePeriod <- loanAmount * interest / 12
-  monthlyAnnuity <- monthlyAnnuityAmount(loanAmount, interest, tenureMonths / 12, amFreeMonths)
+# @param loanAmount the total loan amount including capitalized fees (e.g. startup fee)
+# @param interestRate the annual nominal interest
+# @param tenureMonths the tenure of the loan in number of months
+# @param amortizationFreeMonths the number of initial amortization free months, default to 0
+# @param invoiceFee a fee for each statement invoiced, default to 0
+cashFlow <- function(loanAmount, interestRate, tenureMonths, amortizationFreeMonths, invoiceFee) {
+  interestCostAmFreePeriod <- loanAmount * interestRate / 12
+  monthlyAnnuity <- monthlyAnnuityAmount(loanAmount, interestRate, tenureMonths, amortizationFreeMonths)
   p <- loanAmount * -1.0
   for (month in 1:tenureMonths) {
-    if (amFreeMonths >= month) {
+    if (amortizationFreeMonths >= month) {
       costOfCredit <- interestCostAmFreePeriod
     } else {
       costOfCredit <- monthlyAnnuity
@@ -35,7 +45,13 @@ cashFlow <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoiceFe
   p
 }
 
-paymentPlan <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoiceFee) {
+# @return a dataframe with the initial payment plan based on the input, based on monthly payment periods
+# @param loanAmount the total loan amount including capitalized fees (e.g. startup fee)
+# @param interestRate the annual nominal interest
+# @param tenureMonths the total tenure of the loan in number of months
+# @param amortizationFreeMonths the number of initial amortization free months, default to 0
+# @param invoiceFee a fee for each statement invoiced, default to 0
+paymentPlan <- function(loanAmount, interestRate, tenureMonths, amortizationFreeMonths = 0, invoiceFee = 0) {
   paymentPlans <- data.frame(
     month = numeric(),
     costOfCredit = numeric(),
@@ -45,8 +61,8 @@ paymentPlan <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoic
     outgoingBalance = numeric(),
     cacheFlow = numeric()
   )
-  interestCostAmFreePeriod <- loanAmount * interest / 12
-  monthlyAnnuity <- monthlyAnnuityAmount(loanAmount, interest, tenureMonths, amFreeMonths)
+  interestCostAmFreePeriod <- loanAmount * interestRate / 12
+  monthlyAnnuity <- monthlyAnnuityAmount(loanAmount, interestRate, tenureMonths, amortizationFreeMonths )
   p <- list()
   p$month <- 0
   p$costOfCredit <- 0.0
@@ -57,7 +73,7 @@ paymentPlan <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoic
   p$cacheFlow <- loanAmount * -1.0
 
   paymentPlans <- rbind(paymentPlans, p)
-  prevOB <- p[["outgoingBalance"]]
+  prevOB <- p$outgoingBalance
   for (month in 2:(tenureMonths + 1)) {
     p <- list()
     p$month <- month - 1
@@ -66,21 +82,18 @@ paymentPlan <- function(loanAmount, tenureMonths, amFreeMonths, interest, invoic
     } else {
       p$costOfCredit <- monthlyAnnuity
     }
-    p$interestAmt <- prevOB * interest / 12
+    p$interestAmt <- prevOB * interestRate / 12
     p$amortization <- p$costOfCredit - p$interestAmt
     p$invoiceFee <- invoiceFee
     p$outgoingBalance <- prevOB - p$amortization
     p$cacheFlow <- p$costOfCredit + p$invoiceFee
-    if (month == 1) {
-      str(p)
-    }
     paymentPlans <- rbind(paymentPlans, p)
-    prevOB <- p[["outgoingBalance"]]
+    prevOB <- p$outgoingBalance
   }
   paymentPlans
 }
 # @param monthlyIrr the MONTHLY internal rate of return (monthly irr)
-# @return the annual percentage rate (effektiv ränta)
+# @return the annual percentage rate (effektiv rÃ¤nta)
 apr <- function(monthlyIrr) {
   ((1 + monthlyIrr)^12) - 1
 }
